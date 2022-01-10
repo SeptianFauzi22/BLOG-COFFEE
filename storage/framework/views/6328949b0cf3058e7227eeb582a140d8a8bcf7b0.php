b77012dae92abc96eed28e05a5b4ce6200972aa0
septian fauzi supriyadi <93498752+SeptianFauzi22@users.noreply.github.com>
2022-01-10 14:47:13 +0700

    Add files via upload

---
+++ storage/framework/views/6328949b0cf3058e7227eeb582a140d8a8bcf7b0.php
@@ -0,0 +1,56 @@
+
+<?php $__env->startSection('content'); ?>
+<div class="container">
+      <div class="col-sm">
+        <div class="card" style="width: 90%;">
+            <img src="https://www.superadventure.co.id/uploads/news/2018/10/02/a1207708c16d.jpg" class="card-img-top" alt="gambar laptop">
+            <div class="card-body">
+              <h5 class="card-title">Kopi Arabika</h5>
+              <p class="card-text">Kopi arabika ini adalah kopi pertama yang ditemukan oleh bangsa Arab di Ethiopia. Karena bangsa Arab yang menemukan dan menyebarluaskan ke seluruh dunia maka kopi ini dinamakan kopi arabika.
+
+Kopi arabika ini kopi yang paling populer di dunia. Pedagang kopi di dunia lebih banyak menjual kopi arabika dibandingkan kopi yang populer lainnya. Harga kopi ini juga terbilang mahal. Hal ini disebabkan karena perawatan dan penanamannya yang tidak mudah.
+
+Kopi arabika ini memiliki karakteristiknya sendiri. Kopi ini warna hasil seduhannya tidak terlalu kental. Lalu rasanya pun dari tingkat keasaman kopi lebih tinggi daripada tingkat kafein di dalamnya. Kemudian kopi ini lebih wangi dan lebih kaya daripada kopi lainnya.
+
+Pada kopi arabika ini memiliki varietas lain yang tersebar juga ke seluruh dunia. Berikut adalah varietasnya:
+
+Typica, varietas kopi ini adalah yang tertua yang ditemukan di Ethiopia. Bisa dikatakan bahwa induk dari kopi arabika adalah kopi typica ini. Bahkan kopi pertama yang dibawa oleh Belanda ke Indonesia adalah kopi typica.
+Bourbon, varietas kopi ini ada karena kopi arabika yang dibawa oleh orang Perancis dan ditanam di Pulau Bourbon, sekarang pulau ini bernama Pulau La Reunion. Karena kopi ini beradaptasi dengan tanah di Pulau Bourbon, ia mengalami mutasi dan menjadi varietas kopi bourbon.</p>
+            </div>
+          </div>
+      </div>
+      <div class="col-sm">
+        <div class="card" style="width: 90%;">
+            <img src="https://i0.wp.com/rimbakita.com/wp-content/uploads/2019/03/biji-kopi-robusta.jpg" class="card-img-top" alt="gambar">
+            <div class="card-body">
+              <h5 class="card-title">Kopi Robusta</h5>
+              <p class="card-text">Kopi robusta ini adalah varietas dari kopi canephora. Selain robusta, varietas dari canephora ini adalah kopi nganda. Karena sebagian besar kopi canephora adalah varietas kopi robusta maka nama kopi robusta ini digunakan secara umum untuk kopi canephora.
+
+Kopi robusta ini kuat terhadap hama, sehingga perawatannya pun mudah. Namun kualitas dan harganya lebih rendah daripada kopi arabika. Karena kuatnya kopi ini terhadap hama maka dijuluki robusta, robust memiliki arti kuat.
+
+Robusta ini didatangkan oleh Belanda ke Indonesia untuk menggantikan kopi arabika. Pada masa itu kopi arabika terkena serangan hama karat daun. Karena banyaknya hama dan kopi robusta ini sangat kuat maka kopi robusta yang pada akhirnya di tanam di Indonesia. Oleh karena itu, Indonesia menjadi penghasil kopi robusta terbesar setelah Vietnam dan Brazil. Kopi robusta di Indonesia ada sekitar 80% daripada kopi lainnya.
+
+Karakter kopi robusta ini memiliki aroma yang kuat dan terasa kasar. Rasa dari kopi robusta ini juga lebih pahit daripada kopi lainnya. Oleh karena itu, kopi ini cocok untuk dicampur dengan susu dan coklat saat penyajiannya. Dapat dicampur susu dan coklat karena rasa kopi ini tidak akan kalah dengan rasa lainnya.
+
+Kopi robusta ini memiliki varietas hasil persilangan dengan kopi arabika. Persilangan ini menghasilkan tanaman yang memiliki aroma arabika namun juga daya tahannya kuat seperti robusta. Berikut adalah varietas kopi robusta dan arabika.
+
+Timor, varietas kopi timor ini diambil dari nama tempat untuk pengembangan kopi ini, yaitu Pulau Timor. Persilangan kopi ini bertujuan untuk mengambil rasa dari arabika dan daya tahan dari robusta.
+Catimor, varietas kopi dari persilangan antara caturra dan timor. Varietas persilangan ini bertujuan untuk menghasilkan kopi dengan daya tahan hama seperti timor dan kecepatan panen seperti caturra. Karena timor memiliki rasa robusta, jadi catimor ini memiliki rasa pahit tapi tingkat keasamannya rendah.</p>
+            </div>
+          </div>
+      </div>
+        <div class="col-sm">
+          <div class="card" style="width: 90%;">
+              <img src="https://1.bp.blogspot.com/-1tFanWxVfOM/W2giM8fiduI/AAAAAAAAALI/g7UpbxrrK_UX9SJwmYnHRQkrCQ3x7sZ7QCLcBGAs/s1600/Screenshot_134-compressed.jpg" class="card-img-top" alt="gambar">
+              <div class="card-body">
+              <h5 class="card-title">Kopi Liberika</h5>
+                <p class="card-text">Kopi liberika ini memiliki ukuran lebih besar daripada kopi arabika dan kopi robusta. Namun, setelah selesai dengan proses penjemuran, berat buah kering pada kopi ini hanya 10% dari buah basahnya.
+
+Penyusutan berat kopi liberika sangat tidak disukai oleh petani karena biaya untuk panen menjadi lebih mahal. Karena hal tersebut produksi kopi liberika ini juga tidak sebanyak produksi kopi arabika dan robusta. Tingkat produksi kopi liberika ini hanya sekitar 1% sampai 2% dari kopi dunia.
+
+Pada awalnya kopi liberika ini memiliki nama kopi excelsa yang hanya ada satu spesies. Namun pada tahun 2006 excelsa diresmikan sebagai subspesies dari liberika dan dikenal dengan nama ilmiah coffea liberica var. Dewwrei.</p>
+            </div>
+        </div>
+      </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\blog-coffee\resources\views/index.blade.php ENDPATH**/ ?>
